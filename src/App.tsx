9c4ae641fd8b6636a81e21fb64c53149a45bdd44
wiselee <729360726@qq.com>
2021-08-20 17:59:23 +0800

    style: 首页样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import Header from "./components/header";
+import Mobile from "./components/mobile";
+import Pc from "./components/pc";
+import "./index.css";
 function App() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <Header></Header>
+      <Pc></Pc>
+      <Mobile></Mobile>
+    </div>
+  );
 }
 
 export default App;
